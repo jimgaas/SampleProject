04aef3019ab293149304339f398f9b407243ba70
Jim Gaasedelen <jimgaas@gmail.com>
2021-08-20 04:54:12 -0700

    add number addition input

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [field1, setField1] = useState(0);
+  const [field2, setField2] = useState(0);
+  const [output, setOutput] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{height: 40, fontSize: 30}}
+        placeholder="Field 1"
+        onChangeText={text => setField1(parseFloat(text))}
+      />
+      <TextInput
+        style={{height: 40, fontSize: 30}}
+        placeholder="Field 2"
+        onChangeText={text => setField2(parseFloat(text))}
+      />
+      <Button 
+         title="Submit"
+         onPress={() => setOutput(field1 + field2)}
+        />
+      <Text>The numbers add up to: {output}</Text>
       <StatusBar style="auto" />
     </View>
   );
